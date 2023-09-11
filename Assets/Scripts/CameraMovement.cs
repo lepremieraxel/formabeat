118c474df03786b4ee8f4af1f05f77b1805a4219
ค૪૯Ն <axelmarcial151003@gmail.com>
2023-09-11 16:21:33 +0200

    camera follow player

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,11 +5,5 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    void Start()
-    {
-        Sequence camera = DOTween.Sequence();
-        camera.AppendInterval(3);
-        camera.Append(transform.DOMoveY(29, 10));
-        camera.Append(transform.DOMoveX(-120, 20));
-    }
+  
 }
