3259e232b7ce424219cf8e110269777fa6cad330
ค૪૯Ն <axelmarcial151003@gmail.com>
2023-09-13 16:21:24 +0200

    gameover + win + play

--- Assets/Scripts/ChronoManager.cs
+++ Assets/Scripts/ChronoManager.cs
@@ -8,7 +8,6 @@ public class ChronoManager : MonoBehaviour
 {
     [SerializeField] float timer;
     public Text chrono = null;
-    public SpriteRenderer letterA = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +19,5 @@ public class ChronoManager : MonoBehaviour
     {
         timer += Time.deltaTime;
         chrono.text = Math.Round(timer).ToString();
-        if(Math.Round(timer) % 2 > 0)
-        {
-            letterA.color = Color.cyan;
-        } else
-        {
-            letterA.color = Color.white;
-        }
     }
 }
