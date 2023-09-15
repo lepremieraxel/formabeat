cb782c0c8259f15de05e60df7b85d2a9b3f5f774
oihan desperben <oihandesp@gmail.com>
2023-09-15 21:02:57 +0200

    couleur background fixed et platforms/obstacles animés

--- Assets/Scripts/PlatformAnimatedBehaviour.cs
+++ Assets/Scripts/PlatformAnimatedBehaviour.cs
@@ -12,4 +12,12 @@ public class PlatformAnimatedBehaviour : MonoBehaviour
         mySeq.Append(transform.DOScaleX(8, 0.10f));
         mySeq.Append(transform.DOScaleX(4, 0.38f));
     }
+
+    public void ScaleWithBeat2()
+    {
+        Sequence mySeq1 = DOTween.Sequence();
+        mySeq1.SetLoops(-1);
+        mySeq1.Append(transform.DOScaleY(15, 0.10f));
+        mySeq1.Append(transform.DOScaleY(3, 0.38f));
+    }
 }
